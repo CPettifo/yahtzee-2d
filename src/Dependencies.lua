5521fcfe81bb132837059bf433fe668fa219ec83
Craig Pettifor <craig_petti4@hotmail.com>
2023-02-07 15:55:48 +0100

    add library and dependencies

---
+++ src/Dependencies.lua
@@ -0,0 +1,34 @@
+Class = require 'lib/class'
+
+push = require 'lib/push'
+
+Timer = require 'lib/knife.timer'
+
+
+require 'src/StateMachine'
+
+--require 'src/Board'
+--require 'src/Scorecard'
+
+
+require 'src/states/MenuState'
+require 'src/states/PlayState'
+require 'src/states/RerollState'
+require 'src/states/ResultsState'
+
+gSounds = {
+
+}
+
+gTextures = {
+
+}
+
+
+gFrames = {
+
+}
+
+gFonts = {
+
+}
